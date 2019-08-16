a8315308bc715d9e0fdc6bc5320d6650434f8549
Silvano Cerza <silvanocerza@gmail.com>
2019-08-16 22:26:45 +0200

    Added X11, i3, plasma and systemd

--- link.sh
+++ link.sh
@@ -1,15 +1,44 @@
+#!/bin/bash
+
 # Fonts
-ln -s ./fonts/Monaco\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/Monaco\ Nerd\ Font\ Complete.otf
+mkdir -p ~/.local/share/fonts/
+ln -s -f $(pwd)/fonts/Monaco\ Nerd\ Font\ Complete.otf ~/.local/share/fonts/Monaco\ Nerd\ Font\ Complete.otf
 
 # Git
-ln -s ./git/gitconfig ~/.gitconfig
+ln -s -f $(pwd)/git/gitconfig ~/.gitconfig
+
+# i3
+mkdir -p ~/.config/i3/
+ln -s -f $(pwd)/i3/config ~/.config/i3/config
 
 # Nvim
-ln -s ./nvim/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim/
+ln -s -f $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+
+# Plasma
+mkdir -p ~/.config/plasma-workspace/env/
+ln -s -f $(pwd)/plasma/wm.sh ~/.config/plasma-workspace/env/wm.sh
 
 # Tmux
-ln -s ./tmux/tmux.conf ~/.tmux.conf
+ln -s -f $(pwd)/tmux/tmux.conf ~/.tmux.conf
+
+# Wallpapers
+mkdir -p ~/.wallpaper
+cp $(pwd)/wallpaper/* ~/.wallpaper/
+
+# X11
+ln -s -f $(pwd)/X11/xinitrc ~/.xinitrc
 
 # Zsh
-ln -s ./zsh/zshrc ~/.zshrc
-ln -s ./zsh/oxide.zsh-theme ~/.oh-my-zsh/custom/themes
+mkdir -p ~/.oh-my-zsh/custom/themes/
+ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
+ln -s -f $(pwd)/zsh/oxide.zsh-theme ~/.oh-my-zsh/custom/themes/oxide.zsh-theme
+
+# Systemd
+mkdir -p ~/.config/systemd/user/
+ln -s -f $(pwd)/systemd/wallpaper.service ~/.config/systemd/user/wallpaper.service
+ln -s -f $(pwd)/systemd/wallpaper.timer ~/.config/systemd/user/wallpaper.timer
+
+# Starts systemd services
+systemctl --user enable wallpaper.{service,timer}
+systemctl --user start wallpaper.{service,timer}
